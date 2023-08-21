686d73485b8f1218ea983cac1f2cb6f3c8164973
AlinCuc <alin.cucc@gmail.com>
2023-08-21 16:24:29 +0300

    added mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,99 +1,28 @@
-import React from 'react';
-import { Fragment, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
-import NavBar from './navBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivitiesDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 import LoadingComponent from './LoadingComponents';
-
-
-
+import NavBar from './navBar';
+import ActivitiesDashboard from '../../features/activities/dashboard/ActivitiesDashboard';
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity,setSelectedActivity ] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode]= useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
+  const { activityStore } = useStore();
 
-  useEffect(()=>{
-    agent.Activities.list().then(response=>{
-      let activities: Activity[] = [];
-      response.forEach((acc: Activity) => {
-        acc.date = acc.date.split('T')[0];
-        activities.push(acc)
-      });
-      setActivities(activities) 
-      setLoading (false);
-    })
-  },[])
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore])
 
-  function handleSelectActivity (id:string){
-    setSelectedActivity(activities.find(x=>x.id ===id))
-  }
-  function handleCancelActivity (){
-    setSelectedActivity(undefined);
-  }
-  function handleFormOpen (id?:string){
-    id? handleSelectActivity(id): handleCancelActivity();
-    setEditMode(true);
-  }
-  function handleFormClose(){
-    setEditMode(false);
-  }
-  function handleCreateOrEdiActivity(activity:Activity){
-    setSubmitting(true);
-    if(activity.id){
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x=>x.id !==activity.id), activity])
-        setSelectedActivity(activity)
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }else{
-      activity.id =uuid();
-      agent.Activities.create(activity).then(() =>{
-        setActivities([...activities,activity ]);
-      })
-      setSelectedActivity(activity)
-      setEditMode(false);
-      setSubmitting(false);
-    }
-  }
-  
-  function handleDeleteActivity(id:string){
-    setSubmitting(true);
-    agent.Activities.delete(id).then(()=>{
-      setActivities([...activities.filter(x=>x.id !== id)]);
-      setSubmitting(false);
-    })
-  }
-
-  
-  if(loading){
-   return <LoadingComponent content='Loading app'/>
-  }  
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
 
   return (
     <>
-      <NavBar openForm = {handleFormOpen} />
-      <Container style={{marginTop:'7em'}}>
-      <ActivityDashboard 
-      activities = {activities}
-      selectedActivity ={selectedActivity} 
-      selectActivity = {handleSelectActivity}
-      cancelSelectActivity = {handleCancelActivity}
-      editMode ={editMode}
-      openForm ={handleFormOpen}
-      closeForm ={handleFormClose}
-      createOrEdit = {handleCreateOrEdiActivity}
-      deleteActivity = {handleDeleteActivity}
-      submitting = {submitting}
-      />
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivitiesDashboard />
       </Container>
     </>
   );
 }
-export default App;
+
+export default observer(App);
--- client-app/src/features/details/ActivityDetails.tsx
+++ client-app/src/features/details/ActivityDetails.tsx
@@ -1,32 +1,32 @@
-import { Button, Card, Icon, Image } from 'semantic-ui-react'
-import { Activity } from '../../app/models/activity'
+import {Button, Card, Image} from "semantic-ui-react";
+import LoadingComponent from "../../app/layout/LoadingComponents";
+import { useStore } from "../../app/stores/store";
 
-interface Props {
-    activity :Activity
-    cancelSelectActivity: ()=>void
-    openForm: (id:string) =>void;
 
-}
+export default function ActivityDetails() {
+    const {activityStore} = useStore();
+    const {selectedActivity: activity, openForm, cancelSelectActivity} = activityStore;
 
-export default function ActivityDetails ({activity, cancelSelectActivity, openForm}:Props){
+    if (!activity) return <LoadingComponent />;
+
     return (
-            <Card fluid>
-              <Image src={`\assets/categoryImages/${activity.category}.jpg`}/>
-              <Card.Content>
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+            <Card.Content>
                 <Card.Header>{activity.title}</Card.Header>
                 <Card.Meta>
-                  <span >{activity.date}</span>
+                    <span>{activity.date}</span>
                 </Card.Meta>
                 <Card.Description>
-                  {activity.description}
+                    {activity.description}
                 </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
+            </Card.Content>
+            <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() =>openForm(activity.id)} basic color ='blue' content='Edit'/>
-                    <Button onClick={cancelSelectActivity} basic color ='grey' content='Cancel'/>
-                    </Button.Group>
-              </Card.Content>
-            </Card>
-          )
+                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
+                    <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
 }
